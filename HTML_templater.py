10ee0c9e0522369ee14c3490cd7c45feedcd3a68
Pina Merkert <pina@pinae.net>
2020-01-16 00:47:13 +0100

    Using the TreeProcessor

--- HTML_templater.py
+++ HTML_templater.py
@@ -1,15 +1,25 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, request, Response
 from typing import Union
+from tree_processor import TreeProcessor, IllegalAssetStormStructureError
 import json
 app = Flask(__name__)
 
 
-def build_json_response(content: Union[dict, list], status=200) -> Response:
+def build_json_response(content: Union[dict, list], status: int = 200) -> Response:
     response = app.response_class(
         response=json.dumps(content),
         status=status,
         mimetype='application/json'
+    )
+    return response
+
+
+def build_text_response(content: str, status: int = 200) -> Response:
+    response = app.response_class(
+        response=content,
+        status=status,
+        mimetype='text/plain'
     )
     return response
 
@@ -20,8 +30,13 @@ def template():
         tree = json.loads(request.get_data(as_text=True))
     except json.JSONDecodeError:
         return build_json_response({"error": "The supplied data is not a string in JSON format."}, status=400)
-    return build_json_response({"foo": 2})
+    tp = TreeProcessor()
+    try:
+        rendered_template = tp.run(tree)
+    except IllegalAssetStormStructureError:
+        return build_json_response({"error": "The supplied data is not a valid tree from AssetStorm."}, status=400)
+    return build_text_response(rendered_template)
 
 
 if __name__ == "__main__":  # pragma: no mutate
-    app.run()
+    app.run()  # pragma: no cover
--- tests/test_HTML_templater.py
+++ tests/test_HTML_templater.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from HTML_templater import app
 import unittest
+import responses
+import json
 
 
 class FlaskViewTestCase(unittest.TestCase):
@@ -22,3 +24,33 @@ class FlaskViewTestCase(unittest.TestCase):
             self.assertEqual(400, response.status_code)
         self.assertIn("error", answer)
         self.assertEqual("The supplied data is not a valid tree from AssetStorm.", answer["error"])
+
+    @responses.activate
+    def test_successful_conversion(self):
+        templates = {
+            "article": "<div class=\"article\">{{for(blocks)}}{{blocks}}{{endfor}}</div>",
+            "para": "<p>{{text}}</p>",
+            "list": "<ul>{{for(items)}}<li>{{items}}</li>{{endfor}}</ul>"
+        }
+        for type_name in templates.keys():
+            responses.add(responses.GET, "https://assetstorm.pinae.net/get_template?type_name=" +
+                          type_name + "&template_type=proof_html", status=200, body=templates[type_name])
+        with app.test_client() as test_client:
+            response = test_client.post('/', data=json.dumps({
+                "type": "article",
+                "blocks": [
+                    {"type": "para", "text": "Foo sentence."},
+                    {"type": "list", "items": ["a", "b", "c"]},
+                    {"type": "para", "text": "Bar sentence."}
+                ]
+            }))
+            self.assertEqual('text/plain', response.mimetype)
+            self.assertIn('Content-Type', response.headers)
+            self.assertEqual('text/plain; charset=utf-8', response.headers.get('Content-Type'))
+            answer = str(response.data, encoding='utf-8')
+            self.assertEqual(200, response.status_code)
+        self.assertEqual('<div class="article">' +
+                         '<p>Foo sentence.</p>' +
+                         '<ul><li>a</li><li>b</li><li>c</li></ul>' +
+                         '<p>Bar sentence.</p>' +
+                         '</div>', answer)
