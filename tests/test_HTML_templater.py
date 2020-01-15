28f856da66fd58aee6aa1b139b7f5b05364d1c76
Pina Merkert <pina@pinae.net>
2020-01-15 23:57:16 +0100

    Test for no type in tree

--- tests/test_HTML_templater.py
+++ tests/test_HTML_templater.py
@@ -14,3 +14,11 @@ class FlaskViewTestCase(unittest.TestCase):
             self.assertEqual(400, response.status_code)
         self.assertIn("error", answer)
         self.assertEqual("The supplied data is not a string in JSON format.", answer["error"])
+
+    def test_request_content_no_type(self):
+        with app.test_client() as test_client:
+            response = test_client.post('/', data='{"foo": "bar"}')
+            answer = response.get_json()
+            self.assertEqual(400, response.status_code)
+        self.assertIn("error", answer)
+        self.assertEqual("The supplied data is not a valid tree from AssetStorm.", answer["error"])
